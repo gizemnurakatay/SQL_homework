7dbcc6ee4b7cb0b634c0b81e356a0f13b1a6f30f
Gizem <gizem.bkurt@gmail.com>
2022-01-06 22:31:23 +0300

    odev4sql

---
+++ odev4.sql
@@ -0,0 +1,18 @@
+--1. soru 
+select distinct replacement_cost from film;
+
+--2. soru 
+select count (distinct replacement_cost) from film;
+
+--3.soru 
+select count(*) from film
+where title like 'T%' and rating='G' ;
+
+--4. soru 
+select count(*) from country
+where country like '_____';
+
+--5. soru
+select count (*)  from city
+where city ilike '%r';
+					  
