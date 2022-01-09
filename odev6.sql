bf674946043a6878dcca158fab8c55309f58930f
Gizem <gizem.bkurt@gmail.com>
2022-01-09 16:11:42 +0300

    edit

--- odev6.sql
+++ odev6.sql
@@ -1,1 +1,14 @@
-{\rtf1}
+--1. soru
+select avg(rental_rate) from film;
+
+--2. soru
+select count (*) from film
+where title like 'C%';
+
+--3.soru
+select max(length ) from film
+where rental_rate=0.99 ;
+
+--4.soru
+select count(distinct replacement_cost) from film
+where length >150 ;
