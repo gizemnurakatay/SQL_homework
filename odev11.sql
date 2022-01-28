9075b33780fe5fc71856a70b71e0824f43aa0795
Gizem Nur Bozkurt <akatays@Gizem-MacBook-Air.local>
2022-01-29 00:08:49 +0300

    SQLodev11

---
+++ odev11.sql
@@ -0,0 +1,27 @@
+--1. SORU
+-- (SELECT first_name FROM customer)
+-- UNION
+-- (SELECT first_name FROM actor);
+
+--2.SORU
+--(SELECT first_name FROM customer)
+--INTERSECT
+--(SELECT first_name FROM actor);
+
+--3.SORU
+--(SELECT first_name FROM customer)
+--EXCEPT
+--(SELECT first_name FROM actor);
+
+--4.SORU
+--(SELECT first_name FROM customer)
+--UNION ALL
+--(SELECT first_name FROM actor);
+
+--(SELECT first_name FROM customer)
+--INTERSECT
+--(SELECT first_name FROM actor);
+
+--(SELECT first_name FROM customer)
+--EXCEPT ALL
+--(SELECT first_name FROM actor);
