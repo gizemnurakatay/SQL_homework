5eea82c07aa56beb59ed6b33eca39e5d224fc18e
Gizem Nur Bozkurt <akatays@Gizem-MacBook-Air.local>
2022-01-28 14:27:49 +0300

    odev7sql

---
+++ odev7.sql
@@ -0,0 +1,18 @@
+--1.soru
+--select rating from film
+--group by rating;
+
+--2.soru
+--select replacement_cost, count (*) from film
+--group by replacement_cost
+--having count(*)>50;
+
+--3.soru
+--select store_id, count(*) from customer
+--group by store_id;
+
+--4.soru
+--select country_id, count(*)  from city
+--group by country_id
+--order by count(*) DESC
+--limit 1;
